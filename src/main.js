ebed5ba893789270e23af78f1022756b4a42ad99
Steven Rollason <2099542+gadgetchnnel@users.noreply.github.com>
2022-01-26 16:29:30 +0000

    Increment version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const complexSettings = ['entities', 'state_filter'];
 const entityCards = ['entities', 'glance'];
 
-const TEMPLATER_CARD_VERSION = "0.1.0-alpha2";
+const TEMPLATER_CARD_VERSION = "0.1.0-alpha3";
 
 import { LitElement, html, css } from "lit-element";
 import { createCard, createEntityRow } from "card-tools/src/lovelace-element";
